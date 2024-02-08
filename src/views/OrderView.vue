9577da052e434d737741e1f2762f30c466f1e350
aurebidart <aure.bidart@gmail.com>
2024-02-08 18:24:41 -0300

    Carga las ordenes desde el back y hay que corregir la conexion a WS

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -8,11 +8,13 @@
             <thead>
               <tr>
                 <th></th>
-                <th @click="toggleSort('orden')">Orden
+                <th @click="toggleSort('orden')">
+                  Orden
                   <span v-if="sortKey === 'orden' && sortAsc">↑</span>
                   <span v-else-if="sortKey === 'orden' && !sortAsc">↓</span>
                 </th>
-                <th @click="toggleSort('estado')">Estado
+                <th @click="toggleSort('estado')">
+                  Estado
                   <span v-if="sortKey === 'estado' && sortAsc">↑</span>
                   <span v-else-if="sortKey === 'estado' && !sortAsc">↓</span>
                 </th>
@@ -23,182 +25,303 @@
               </tr>
             </thead>
             <tbody>
-              <tr v-for="(order, index) in sortedOrders" :key="index" :class="{ 'selected-row': order.selected }">
-                <td><input type="checkbox" v-model="order.selected"></td>
-                <td>{{ order.orden }}</td>
+              <tr
+                v-for="(order, index) in sortedOrders"
+                :key="index"
+                :class="{ 'selected-row': order.selected }"
+              >
+                <td><input type="checkbox" v-model="order.selected" /></td>
+                <td>{{ order.id }}</td>
                 <td>{{ order.estado }}</td>
-                <td>{{ order.camion }}</td>
+                <td>{{ order.camion.patente }}</td>
                 <td>{{ order.preset }}</td>
-                <td>{{ order.chofer }}</td>
-                <td>{{ order.producto }}</td>
+                <td>{{ order.chofer.apellido }}</td>
+                <td>{{ order.producto.nombre }}</td>
               </tr>
             </tbody>
           </table>
         </div>
         <!-- Botones -->
-        <div class="d-flex flex-column flex-shrink-0 ml-3 mt-5 button-container">
-          <button class="btn custom-button mb-2" @click="openModal('pesarCamionModal')">
+        <div
+          class="d-flex flex-column flex-shrink-0 ml-3 mt-5 button-container"
+        >
+          <button
+            class="btn custom-button mb-2"
+            @click="openModal('pesarCamionModal')"
+          >
             Pesar Camion
           </button>
-          <button class="btn custom-button mb-2" @click="openModal('finalizarCargaModal')">
+          <button
+            class="btn custom-button mb-2"
+            @click="openModal('finalizarCargaModal')"
+          >
             Finalizar Carga
           </button>
-          <button class="btn custom-button mb-2" @click="openModal('pesajeFinalModal')">
+          <button
+            class="btn custom-button mb-2"
+            @click="openModal('pesajeFinalModal')"
+          >
             Pesaje Final
           </button>
-          <button class="btn custom-button" @click="openModal('verConciliacionModal')">
+          <button
+            class="btn custom-button"
+            @click="openModal('verConciliacionModal')"
+          >
             Ver Conciliación
           </button>
         </div>
       </div>
     </div>
-    <button class="btn custom-button fixed-bottom-center" @click="openModal('nuevaOrdenModal', false)">Nueva Orden</button>
+    <button
+      class="btn custom-button fixed-bottom-center"
+      @click="openModal('nuevaOrdenModal', false)"
+    >
+      Nueva Orden
+    </button>
 
     <!-- Modal para pesar el camión -->
-    <div v-if="selectedOrder" class="modal" id="pesarCamionModal" tabindex="-1" aria-labelledby="pesarCamionModalLabel" aria-hidden="true">
+    <div
+      v-if="selectedOrder"
+      class="modal"
+      id="pesarCamionModal"
+      tabindex="-1"
+      aria-labelledby="pesarCamionModalLabel"
+      aria-hidden="true"
+    >
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title">Pesar Camión</h5>
-            <button type="button" class="btn-close" @click="closeModal('pesarCamionModal')"></button>
+            <button
+              type="button"
+              class="btn-close"
+              @click="closeModal('pesarCamionModal')"
+            ></button>
           </div>
           <div class="modal-body">
             <h6>Orden: {{ selectedOrder.orden }}</h6>
             <h6>Camión: {{ selectedOrder.camion }}</h6>
             <div class="form-group">
               <label for="pesoInput">Peso:</label>
-              <input type="text" class="form-control" id="pesoInput" v-model="pesoTexto" placeholder="Ingrese el peso...">
+              <input
+                type="text"
+                class="form-control"
+                id="pesoInput"
+                v-model="pesoTexto"
+                placeholder="Ingrese el peso..."
+              />
             </div>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-primary" @click="listoClick">Listo</button>
-            <button type="button" class="btn btn-secondary" @click="closeModal('pesarCamionModal')">Cerrar</button>
+            <button type="button" class="btn btn-primary" @click="listoClick">
+              Listo
+            </button>
+            <button
+              type="button"
+              class="btn btn-secondary"
+              @click="closeModal('pesarCamionModal')"
+            >
+              Cerrar
+            </button>
           </div>
         </div>
       </div>
     </div>
 
-<!-- Modal para finalizar carga -->
-<div v-if="selectedOrder" class="modal" id="finalizarCargaModal" tabindex="-1" aria-labelledby="finalizarCargaModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <!-- Cabecera del modal -->
-      <div class="modal-header">
-        <h5 class="modal-title" id="finalizarCargaModalLabel">Finalizar Carga</h5>
-        <button type="button" class="btn-close" @click="closeModal('pesarCamionModal')"></button>
-      </div>
+    <!-- Modal para finalizar carga -->
+    <div
+      v-if="selectedOrder"
+      class="modal"
+      id="finalizarCargaModal"
+      tabindex="-1"
+      aria-labelledby="finalizarCargaModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <!-- Cabecera del modal -->
+          <div class="modal-header">
+            <h5 class="modal-title" id="finalizarCargaModalLabel">
+              Finalizar Carga
+            </h5>
+            <button
+              type="button"
+              class="btn-close"
+              @click="closeModal('pesarCamionModal')"
+            ></button>
+          </div>
 
-      <!-- Cuerpo del modal -->
-      <div class="modal-body text-center">
-        <p>Se cargaron {{ X }} kg de {{ Y }} kg.</p>
-      </div>
+          <!-- Cuerpo del modal -->
+          <div class="modal-body text-center">
+            <p>Se cargaron {{ X }} kg de {{ Y }} kg.</p>
+          </div>
 
-      <!-- Pie del modal -->
-      <div class="modal-footer justify-content-center">
-        <button type="button" class="btn btn-primary" @click="listoClick">Aceptar</button>
-        <button type="button" class="btn btn-secondary" @click="closeModal('finalizarCargaModal')">Cancelar</button>
+          <!-- Pie del modal -->
+          <div class="modal-footer justify-content-center">
+            <button type="button" class="btn btn-primary" @click="listoClick">
+              Aceptar
+            </button>
+            <button
+              type="button"
+              class="btn btn-secondary"
+              @click="closeModal('finalizarCargaModal')"
+            >
+              Cancelar
+            </button>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-
-
-
 
     <!-- Modal para pesaje final -->
-    <div v-if="selectedOrder" class="modal" id="pesajeFinalModal" tabindex="-1" aria-labelledby="pesajeFinalModalLabel" aria-hidden="true">
+    <div
+      v-if="selectedOrder"
+      class="modal"
+      id="pesajeFinalModal"
+      tabindex="-1"
+      aria-labelledby="pesajeFinalModalLabel"
+      aria-hidden="true"
+    >
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title">Pesaje Final</h5>
-            <button type="button" class="btn-close" @click="closeModal('pesajeFinalModal')"></button>
+            <button
+              type="button"
+              class="btn-close"
+              @click="closeModal('pesajeFinalModal')"
+            ></button>
           </div>
           <div class="modal-body">
             <h6>Orden: {{ selectedOrder.orden }}</h6>
             <h6>Camión: {{ selectedOrder.camion }}</h6>
             <div class="form-group">
               <label for="pesoInput">Peso:</label>
-              <input type="text" class="form-control" id="pesoInput" v-model="pesoTexto" placeholder="Ingrese el peso...">
+              <input
+                type="text"
+                class="form-control"
+                id="pesoInput"
+                v-model="pesoTexto"
+                placeholder="Ingrese el peso..."
+              />
             </div>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-primary" @click="listoClick">Listo</button>
-            <button type="button" class="btn btn-secondary" @click="closeModal('pesajeFinalModal')">Cerrar</button>
+            <button type="button" class="btn btn-primary" @click="listoClick">
+              Listo
+            </button>
+            <button
+              type="button"
+              class="btn btn-secondary"
+              @click="closeModal('pesajeFinalModal')"
+            >
+              Cerrar
+            </button>
           </div>
         </div>
       </div>
     </div>
 
     <!-- Modal para ver conciliación -->
-    <div v-if="selectedOrder" class="modal" id="verConciliacionModal" tabindex="-1" aria-labelledby="verConciliacionModalLabel" aria-hidden="true">
+    <div
+      v-if="selectedOrder"
+      class="modal"
+      id="verConciliacionModal"
+      tabindex="-1"
+      aria-labelledby="verConciliacionModalLabel"
+      aria-hidden="true"
+    >
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <!-- Contenido del modal para ver conciliación, falta hacer -->
         </div>
       </div>
     </div>
-    
-  <!-- Modal para nueva orden -->
-<div class="modal" id="nuevaOrdenModal" tabindex="-1" aria-labelledby="nuevaOrdenModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="nuevaOrdenModalLabel">Nueva Orden</h5>
-        <button type="button" class="btn-close" @click="closeModal('nuevaOrdenModal')"></button>
-      </div>
 
-      <div class="modal-body">
-          <div class="mb-3">
-            <label for="choferInput" class="form-label">Chofer</label>
-            <select class="form-select" id="choferInput">
-              <option selected>Seleccionar Chofer...</option>
-              </select>
+    <!-- Modal para nueva orden -->
+    <div
+      class="modal"
+      id="nuevaOrdenModal"
+      tabindex="-1"
+      aria-labelledby="nuevaOrdenModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="nuevaOrdenModalLabel">Nueva Orden</h5>
+            <button
+              type="button"
+              class="btn-close"
+              @click="closeModal('nuevaOrdenModal')"
+            ></button>
           </div>
 
-          <div class="mb-3">
-            <label for="clienteInput" class="form-label">Cliente</label>
-            <select class="form-select" id="clienteInput">
-              <option selected>Seleccionar Cliente...</option>
+          <div class="modal-body">
+            <div class="mb-3">
+              <label for="choferInput" class="form-label">Chofer</label>
+              <select class="form-select" id="choferInput">
+                <option selected>Seleccionar Chofer...</option>
               </select>
+            </div>
+
+            <div class="mb-3">
+              <label for="clienteInput" class="form-label">Cliente</label>
+              <select class="form-select" id="clienteInput">
+                <option selected>Seleccionar Cliente...</option>
+              </select>
+            </div>
+
+            <div class="mb-3">
+              <label for="camionInput" class="form-label">Camión</label>
+              <select class="form-select" id="camionInput">
+                <option selected>Seleccionar Camión...</option>
+              </select>
+            </div>
+
+            <div class="mb-3">
+              <label for="productoInput" class="form-label">Producto</label>
+              <select class="form-select" id="productoInput">
+                <option selected>Seleccionar Producto...</option>
+              </select>
+            </div>
+
+            <div class="mb-3">
+              <label for="fechaInput" class="form-label">Fecha</label>
+              <datepicker
+                v-model="fechaSeleccionada"
+                :language="es"
+              ></datepicker>
+            </div>
           </div>
 
-          <div class="mb-3">
-            <label for="camionInput" class="form-label">Camión</label>
-            <select class="form-select" id="camionInput">
-              <option selected>Seleccionar Camión...</option>
-              </select>
-          </div>
-
-          <div class="mb-3">
-            <label for="productoInput" class="form-label">Producto</label>
-            <select class="form-select" id="productoInput">
-              <option selected>Seleccionar Producto...</option>
-              </select>
-          </div>
-
-          <div class="mb-3">
-            <label for="fechaInput" class="form-label">Fecha</label>
-            <datepicker v-model="fechaSeleccionada" :language="es"></datepicker>
+          <div class="modal-footer justify-content-center">
+            <button type="button" class="btn btn-primary" @click="listoClick">
+              Aceptar
+            </button>
+            <button
+              type="button"
+              class="btn btn-secondary"
+              @click="closeModal('nuevaOrdenModal')"
+            >
+              Cancelar
+            </button>
           </div>
         </div>
-
-      <div class="modal-footer justify-content-center">
-        <button type="button" class="btn btn-primary" @click="listoClick">Aceptar</button>
-        <button type="button" class="btn btn-secondary" @click="closeModal('nuevaOrdenModal')">Cancelar</button>
       </div>
     </div>
-  </div>
-</div>
 
-<div v-if="modalBackdropVisible" class="modal-backdrop"></div>
+    <div v-if="modalBackdropVisible" class="modal-backdrop"></div>
   </div>
 </template>
 
 <script>
-import CarouselComponent from '@/components/CarouselComponent.vue';
-import { toast } from 'vue3-toastify';
-import 'vue3-toastify/dist/index.css';
-import Datepicker from 'vue3-datepicker';
+import CarouselComponent from "@/components/CarouselComponent.vue";
+import { toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
+import Datepicker from "vue3-datepicker";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 export default {
   components: {
@@ -207,28 +330,39 @@ export default {
   },
   data() {
     return {
-      orders: this.generateRandomOrders(100),
-      sortKey: 'estado',
+      orders: [],
+      sortKey: "estado",
       sortAsc: true,
       selectedOrder: null,
-      pesoTexto: '',
+      pesoTexto: "",
       modalBackdropVisible: false,
       fechaSeleccionada: new Date(),
     };
   },
+  mounted() {
+    this.obtainOrders().then((data) => {
+      this.orders = data;
+    });
+  },
   computed: {
     sortedOrders() {
-      return [...this.orders].sort((a, b) => (this.sortAsc ? a[this.sortKey] - b[this.sortKey] : b[this.sortKey] - a[this.sortKey]));
+      return [...this.orders].sort((a, b) =>
+        this.sortAsc
+          ? a[this.sortKey] - b[this.sortKey]
+          : b[this.sortKey] - a[this.sortKey]
+      );
     },
   },
   methods: {
     openModal(modalId, validateSelection = true) {
       if (validateSelection) {
-        const selectedOrders = this.sortedOrders.filter(order => order.selected);
+        const selectedOrders = this.sortedOrders.filter(
+          (order) => order.selected
+        );
 
         if (selectedOrders.length !== 1) {
-          toast.error('Se debe seleccionar 1 orden obligatoriamente', {
-            timeout: 5000
+          toast.error("Se debe seleccionar 1 orden obligatoriamente", {
+            timeout: 5000,
           });
           return;
         }
@@ -239,35 +373,20 @@ export default {
 
       this.$nextTick(() => {
         const modal = document.getElementById(modalId);
-        modal.classList.add('show');
+        modal.classList.add("show");
       });
     },
 
     closeModal(modalId) {
       this.selectedOrder = null;
-      this.pesoTexto = '';
+      this.pesoTexto = "";
       const modal = document.getElementById(modalId);
-      modal.classList.remove('show');
+      modal.classList.remove("show");
       this.modalBackdropVisible = false;
     },
     listoClick() {
-      console.log('Peso ingresado:', this.pesoTexto);
-      this.closeModal('pesarCamionModal');
-    },
-    generateRandomOrders(count) {
-      const orders = [];
-      for (let i = 0; i < count; i++) {
-        orders.push({
-          orden: Math.floor(Math.random() * 1000),
-          estado: [1, 2, 3, 4][Math.floor(Math.random() * 4)],
-          camion: `Camión ${Math.floor(Math.random() * 100)}`,
-          preset: `Preset ${Math.floor(Math.random() * 100)}`,
-          chofer: `Chofer ${Math.floor(Math.random() * 100)}`,
-          producto: `Producto ${Math.floor(Math.random() * 100)}`,
-          selected: false,
-        });
-      }
-      return orders;
+      console.log("Peso ingresado:", this.pesoTexto);
+      this.closeModal("pesarCamionModal");
     },
     toggleSort(key) {
       if (this.sortKey === key) {
@@ -275,6 +394,23 @@ export default {
       } else {
         this.sortKey = key;
         this.sortAsc = true;
+      }
+    },
+    async obtainOrders() {
+      try {
+        const response = await axios.get(
+          "http://localhost:9080/api/v1/orders/list",
+          {
+            headers: {
+              Authorization: "Bearer " + Cookies.get("token"),
+            },
+          }
+        );
+        console.log("Órdenes obtenidas:", response.data);
+        return response.data;
+      } catch (error) {
+        console.error("Error al obtener las órdenes:", error);
+        return [];
       }
     },
   },
@@ -332,7 +468,6 @@ export default {
   height: 100%;
 }
 #nuevaOrdenModal {
-  top: -20px;  
+  top: -20px;
 }
-
 </style>
